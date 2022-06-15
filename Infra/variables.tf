1edae637fa6af7c85b77c1a67c47b34f2c9d8700
P <saranya.c.p@capgemini.com>
2022-06-15 06:49:44 +0530

    Added Files

---
+++ Infra/variables.tf
@@ -0,0 +1,36 @@
+# Copyright (c) 2021 Microsoft
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+variable "resource_group" {
+  default = "aml-terraform-demo"
+}
+
+variable "workspace_display_name" {
+  default = "aml-terraform-demo"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+
+variable "jumphost_username" {
+  default = "azureuser"
+}
+
+variable "jumphost_password" {
+  default = "ThisIsNotVerySecure!"
+}
+
+variable "prefix" {
+  type = string
+  default = "aml"
+}
+
+resource "random_string" "postfix" {
+  length = 6
+  special = false
+  upper = false
+}
